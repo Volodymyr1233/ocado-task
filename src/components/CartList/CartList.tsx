04523de299c329c4934011e08128ae6b57112cee
Volodymyr1233 <105359387+Volodymyr1233@users.noreply.github.com>
2025-05-10 22:00:41 +0200

    created summary_page with basic structure

--- src/components/CartList/CartList.tsx
+++ src/components/CartList/CartList.tsx
@@ -1,9 +1,12 @@
 import { useCartItems } from "hooks/useCartItems";
 import styles from "./CartList.module.css";
 import CartsItem from "components/CartItem/CartItem";
+import { useNavigate } from "react-router-dom";
 
 export default function CartList() {
   const { cartItems, calculateTotalPrice } = useCartItems();
+  const navigate = useNavigate();
+
   return (
     <section className={styles.cartListMainContainer}>
       <h1>Koszyk</h1>
@@ -16,6 +19,7 @@ export default function CartList() {
       <h2 className={styles.cartItemsTotalPrice}>
         Łączna kwota: {calculateTotalPrice()}
       </h2>
+
     </section>
   );
 }
