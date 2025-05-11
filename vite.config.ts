1479f08a6562ff8b79f04a07ff20651d6a31a193
Volodymyr1233 <105359387+Volodymyr1233@users.noreply.github.com>
2025-05-11 19:50:33 +0200

    vite config build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        order: resolve(__dirname, "order.html"),
+      },
+    },
+  },
   resolve: {
     alias: [
       {
